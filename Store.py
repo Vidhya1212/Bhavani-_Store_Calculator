eef9e34c3c05cc9ff7433c5fcae6db9c830168ab
Vidhya1212 <preshan.12561212@gmail.com>
2026-09-18 11:06:17 +0530

    Update Store.py

--- Store.py
+++ Store.py
@@ -2,7 +2,7 @@
 # # Bhavani Store - Billing Calculator
 # My first Python project - Built on mobile
 
-storename = "Bhavani"
+storename = "Bhavani Stationary Shop"
 print(f"Welcome to {storename} Store")
 n=int(input("How may customers today:"))
      
@@ -16,10 +16,16 @@ for customer in range(n):
 
     print(f"You pay: {finalprice}")
 
-    if finalprice > 1000:
-        print("Wow! FREE Gift! 🎁")
+    if finalprice > 1500:
+        print("Bumper Offer! FREE School Bag! 🛒")
+    elif finalprice > 1000:
+        print("Super! FREE Water Bottle! 🧴")
+    elif finalprice > 500:
+        print("Nice! FREE Pen Set! 🖊️")
+    elif finalprice > 100:
+        print("Free Chocolate! 🍫")
     else:
         print("Thank you! Visit again!")
 
 # LOOP ENDS
-print("===Welcome===")
+print("===Welcome!===")
